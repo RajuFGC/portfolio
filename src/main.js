63f05a838d17c470427b072ee81d1f564da35c9b
RajuFGC <140109427+RajuFGC@users.noreply.github.com>
2023-11-28 11:14:56 +0100

    added image optimization

--- src/main.js
+++ src/main.js
@@ -13,13 +13,14 @@ fetch(`https://api.github.com/users/${username}/repos?direction=${direction}`)
     for (let tag of topics) {
         tag += `<li class="bg-gray-400/10 py-1 px-4 rounded font-bold text-sm">${tag}</li>`
     }
-    const element = `<article class="rounded-rajuborderXL overflow-clip bg-gradient-to-br from-white/10 to white/5">
+    const element = `<article class="rounded-rajuborderXL overflow-clip bg-gradient-to-br from-white/10 to white/5 flex flex-col h-full">
     <div class="border-b border-bg h-11 p-4 flex gap-1.5 shadow-innerLight rounded-t-rajuBorder bg-gradient-to-br from-white/10 to white/5 ">
         <span class="w-3 h-3 block rounded-full bg-bg opacity-50"></span>
         <span class="w-3 h-3 block rounded-full bg-bg opacity-50"></span>
         <span class="w-3 h-3 block rounded-full bg-bg opacity-50"></span>
     </div>
-    <div class="p-5 md:p-6 lg:p-10">
+    <div class="p-5 md:p-6 lg:p-10 flex flex-col justify-between grow">
+    <div>
     <header class="flex gap-4 items-center mb-4">
     <h3 class="font-bold text-2xl leading-none">${name}</h3>
     <p class="bg-gray-400/10 py-1 px-4 rounded font-medium leading-none flex items-center gap-0.5 "><img src="${smallStar}" alt="image of a star" class="w-4 h-4">${stargazers_count}</p>
@@ -28,9 +29,10 @@ fetch(`https://api.github.com/users/${username}/repos?direction=${direction}`)
     <ul class="flex gap-2 mb-10 flex-wrap">
     ${tags}
     </ul>
-    <div class="flex flex-col md:flex-row items-start gap-4">
-    <a target ="_blank" rel="noreferrer nofollow" class="bg-bg border-stroke border-2 text-accent flex gap-3 rounded-xl font-bold text-base md:text-xl py-4 px-5 items-center"  href="${homepage}"><img src="${boxIcon}" class="w-6 h-6" alt="">View demo</a>
-    <a target ="_blank" rel="noreferrer nofollow" class="bg-bg border-stroke border-2 text-accent flex gap-3 rounded-xl font-bold text-base md:text-xl py-4 px-5 items-center" href="${html_url}"><img src="${gitHubIcon}" class="w-6 h-6" alt="">Source code</a>
+    </div>
+    <div class="flex flex-col flex-wrap md:flex-row items-start gap-4">
+    <a target ="_blank" rel="noreferrer nofollow" class="bg-bg border-stroke border-2 hover:border-accent-500 transition-colors duration-500 text-accent flex gap-3 rounded-xl font-bold text-base md:text-xl py-4 px-5 items-center"  href="${homepage}"><img src="${boxIcon}" class="w-6 h-6" alt="">View demo</a>
+    <a target ="_blank" rel="noreferrer nofollow" class="bg-bg border-stroke border-2 hover:border-accent-500 transition-colors duration-500 text-accent flex gap-3 rounded-xl font-bold text-base md:text-xl py-4 px-5 items-center" href="${html_url}"><img src="${gitHubIcon}" class="w-6 h-6" alt="">Source code</a>
 </div>
 </div>
 </article>`;
