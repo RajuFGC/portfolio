68ea4bd3d9c2331778b020922eb58a6c48560ae8
RajuFGC <140109427+RajuFGC@users.noreply.github.com>
2023-11-28 14:33:18 +0100

    fixed some issues

--- src/main.js
+++ src/main.js
@@ -13,17 +13,17 @@ fetch(`https://api.github.com/users/${username}/repos?direction=${direction}`)
     for (let tag of topics) {
         tag += `<li class="bg-gray-400/10 py-1 px-4 rounded font-bold text-sm">${tag}</li>`
     }
-    const element = `<article class="rounded-rajuborderXL overflow-clip bg-gradient-to-br from-white/10 to white/5 flex flex-col h-full">
-    <div class="border-b border-bg h-11 p-4 flex gap-1.5 shadow-innerLight rounded-t-rajuBorder bg-gradient-to-br from-white/10 to white/5 ">
+    const element = `<article class="rounded-rajuborder md:rounded-rajuborderXL overflow-clip bg-gradient-to-br from-white/10 to white/5 flex flex-col h-full">
+    <div class=" h-11 p-4 flex gap-1.5 shadow-innerLight rounded-t-rajuBorder md:rounded-t-rajuborderXL bg-gradient-to-br from-white/10 to white/5 ring-1 ring-inset ring-bg ">
         <span class="w-3 h-3 block rounded-full bg-bg opacity-50"></span>
         <span class="w-3 h-3 block rounded-full bg-bg opacity-50"></span>
         <span class="w-3 h-3 block rounded-full bg-bg opacity-50"></span>
     </div>
-    <div class="p-5 md:p-6 lg:p-10 flex flex-col justify-between grow">
+    <div class="p-5 md:p-6 lg:p-10 flex flex-col justify-between grow ring-1 ring-inset ring-bg">
     <div>
     <header class="flex gap-4 items-center mb-4">
     <h3 class="font-bold text-2xl leading-none">${name}</h3>
-    <p class="bg-gray-400/10 py-1 px-4 rounded font-medium leading-none flex items-center gap-0.5 "><img src="${smallStar}"  alt="image of a star" class="w-4 h-4">${stargazers_count}</p>
+    <p class="bg-gray-400/10 py-1 px-4 rounded font-medium leading-none flex items-center gap-0.5 "><img src="${smallStar}" alt="image of a star" class="w-4 h-4">${stargazers_count}</p>
     </header>
     <p class="text-gray-400 font-normal text-xl leading-6 mb-4">${description}</p>
     <ul class="flex gap-2 mb-10 flex-wrap">
